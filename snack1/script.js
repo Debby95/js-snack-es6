2b5650c8467d8d4c4a9708872440d1315efb7313
Debby95 <debbyjacksonstar@hotmail.it>
2022-06-06 15:12:57 +0200

    + array che descrive le bici da corsa

---
+++ snack1/script.js
@@ -0,0 +1,27 @@
+// creo un array di oggetti che descrive le bici da corsa
+const bici = [ 
+    {
+        nome: "Wilier 0 SLR",
+        peso: 6.5,
+    },
+    {
+        nome: "Cannondale SuperSix EVOHi-MOD Disc Dura Ace",
+        peso: 8.0,
+    },
+    {
+        nome: "Trek Emonda 2020",
+        peso: 6.81,
+    },
+    {
+        nome: "Trek Emonda SLR",
+        peso: 4.65,
+    },
+    {
+        nome: "SAVA Phantom 2.0 700C",
+        peso: 8.1,
+    },
+];
+
+//console.table(bici);
+
+
