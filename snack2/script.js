8139b2faaaed1c7d2f81f4cb8efb9d3c9cd51fd6
Debby95 <debbyjacksonstar@hotmail.it>
2022-06-06 23:58:29 +0200

    + squadre calcio e  stampa in console pt tot + falli subiti

---
+++ snack2/script.js
@@ -0,0 +1,47 @@
+//creo un array di oggetti di squadre di calcio e ognuna di loro avrà nome, punti fatti e falli subiti
+const footballSquad = [
+    {
+        nome: "Inter",
+        puntiTot: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Milan",
+        puntiTot: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Juve",
+        puntiTot: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Roma",
+        puntiTot: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Napoli",
+        puntiTot: 0,
+        falliSubiti: 0,
+    },
+];
+
+console.table(footballSquad);
+
+//genero numeri random per punti tot e falli subiti
+for (let i = 1; i <= footballSquad.length; i++) {
+    
+    //creo numeri random per punti tot
+    footballSquad.puntiTot = Math.floor(Math.random() * 10);
+
+    //creo numeri random per falli subiti
+    footballSquad.fallitiSubiti = Math.floor(Math.random() * 5);
+
+    //stampo in consoe i punti tot e i falli subiti
+    console.table(footballSquad.puntiTot, footballSquad.fallitiSubiti);
+};
+
+//creo un arrey usando la destructuring i cui elementi contengono solo nomi e falli subiti
+
+
