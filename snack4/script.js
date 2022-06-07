f405afead482b71e3357312e30f638dab69d5b5e
Debby95 <debbyjacksonstar@hotmail.it>
2022-06-07 15:51:36 +0200

    + nuovo arrey con i nomi degli studenti in maiuscolo

---
+++ snack4/script.js
@@ -0,0 +1,45 @@
+//creo un elenco studenti di una facoltà con il tot dei loro voti
+let students = [
+    {   name: "Marco", 
+        id: 213, 
+        grades: 78 
+    },
+    {   name: "Paola", 
+        id: 110, 
+        grades: 96 
+    },
+    {   name: "Andrea", 
+        id: 250, 
+        grades: 48 
+    },
+    {   name: "Gaia", 
+        id: 145, 
+        grades: 74 
+    },
+    {   name: "Luigi", 
+        id: 196, 
+        grades: 68 
+    },
+    {   name: "Piero", 
+        id: 102, 
+        grades: 50 
+    },
+    {   name: "Francesca", 
+        id: 120, 
+        grades: 84 
+    },
+];
+
+//console.table(students);
+
+//+ nuovo arrey con i nomi degli studenti in maiuscolo
+const studentsList = students.map((students) => {
+    
+    students = students.name.toUpperCase();
+
+    return {
+        students,
+    }
+})
+
+console.table(studentsList);
